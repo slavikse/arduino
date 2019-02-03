f5e7346c6e6f6b2d64a5d2cfc2874268cfb6a3bc
slavikse <gslavikse@gmail.com>
2019-02-03 19:36:02 +0300

    20_rope_pulling

---
+++ sketch/20_rope_pulling/20_rope_pulling.ino
@@ -0,0 +1,52 @@
+const int BUZZER_PIN = 0;
+const int FIRST_BAR_PIN = 4;
+const int BAR_COUNT = 10;
+const int MAX_SCORE = 20;
+
+volatile int score = 0;
+
+void setup()
+{
+  for (int i = 0; i < BAR_COUNT; i++)
+  {
+    pinMode(FIRST_BAR_PIN + i, OUTPUT);
+  }
+
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  attachInterrupt(INT1, pushP1, FALLING);
+  attachInterrupt(INT0, pushP2, FALLING);
+}
+
+void pushP1()
+{
+  score += 1;
+}
+
+void pushP2()
+{
+  score -= 1;
+}
+
+void loop()
+{
+  tone(BUZZER_PIN, 2000, 1000);
+
+  while (abs(score) < MAX_SCORE)
+  {
+    int bound = map(score, -MAX_SCORE, MAX_SCORE, 0, BAR_COUNT);
+    int left = min(bound, BAR_COUNT / 2 - 1);
+    int right = max(bound, BAR_COUNT / 2);
+
+    for (int i = 0; i < BAR_COUNT; i++)
+    {
+      digitalWrite(FIRST_BAR_PIN + i, i >= left && i <= right);
+    }
+  }
+
+  tone(BUZZER_PIN, 4000, 1000);
+
+  // while (true)
+  // {
+  // }
+}
