f10ed7107a983b02ae08ffce450252d459912373
slavikse <gslavikse@gmail.com>
2019-02-03 17:28:22 +0300

    19_lamp_controlled_by_usb

---
+++ retro/game.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+int step = 0;
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.print("Hello!");
+}
+
+void loop()
+{
+  lcd.setCursor(step, 1);
+  lcd.print("-");
+
+  delay(200);
+
+  lcd.clear();
+  step = (step + 2) % 16;
+}
